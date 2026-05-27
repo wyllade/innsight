ff45c7ce9c813d07e430d3418fa7b3f5cc9c1257
Halima <h4limaabdi@gmail.com>
2026-05-27 16:54:04 +0300

    feat: implement auth system and hotel CRUD base

--- backend/app/routes/hotel.py
+++ backend/app/routes/hotel.py
@@ -44,3 +44,51 @@ def get_single_hotel(id):
     hotel = Hotel.query.get_or_404(id)
 
     return jsonify(hotel.to_dict()), 200
+
+
+@hotel_bp.route("/<int:id>", methods=["PUT"])
+@jwt_required()
+def update_hotel(id):
+    hotel = Hotel.query.get_or_404(id)
+
+    current_user = get_jwt_identity()
+
+    if hotel.created_by != current_user:
+        return jsonify({
+            "error": "Unauthorized"
+        }), 403
+
+    data = request.get_json()
+
+    hotel.name = data["name"]
+    hotel.location = data["location"]
+    hotel.description = data["description"]
+    hotel.price_per_night = data["price_per_night"]
+
+    db.session.commit()
+
+    return jsonify({
+        "message": "Hotel updated",
+        "hotel": hotel.to_dict()
+    }), 200
+
+
+@hotel_bp.route("/<int:id>", methods=["DELETE"])
+@jwt_required()
+def delete_hotel(id):
+    hotel = Hotel.query.get_or_404(id)
+
+    current_user = get_jwt_identity()
+
+    if hotel.created_by != current_user:
+        return jsonify({
+            "error": "Unauthorized"
+        }), 403
+
+    db.session.delete(hotel)
+
+    db.session.commit()
+
+    return jsonify({
+        "message": "Hotel deleted"
+    }), 200
